dce6417767f74c5d9d146a7e3cb7feab2f384db2
dcebeci <cebecidogukan@gmail.com>
2022-05-19 22:38:45 +0300

    cart eklendi.

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -4,12 +4,6 @@ import Grid from '@material-ui/core/Grid';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products= [
-  { id: 1, name: 'Shoes', description: 'Running Shoes.', price: '1500₺', image:'https://s2.r29static.com/bin/entry/ebd/0,675,2000,1050/x,80/1929471/image.jpg'},
-  { id: 2, name: 'Shoes', description: 'Running Shoes.', price: '1800₺', image:'https://i.insider.com/5e84e28f2d654f2c2469cc6b?width=1136&format=jpeg'},
-  
-];
-
 const Products = ({ products, onAddToCart }) => {
   const classes = useStyles();
 
