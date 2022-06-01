203a21f25addcc10a72650cb8c8185f8a0d49d02
dcebeci <cebecidogukan@gmail.com>
2022-06-01 23:28:17 +0300

    .

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -8,7 +8,7 @@ import useStyles from './styles';
 const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart , handleUpdateCartQty, handleEmptyCart, handleRemoveFromCart }) => {
     const classes = useStyles();
   
-
+.....
   
     const handleEmptyCart = () => onEmptyCart();
   
