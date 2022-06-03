96207f553111576cb7f82f3f280746c4c13cdae7
dcebeci <cebecidogukan@gmail.com>
2022-06-03 22:50:21 +0300

    checkout

--- src/Components/CheckoutForm/Checkout/Checkout.jsx
+++ src/Components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button} from '@material-ui/core'
 import useStyles from './styles';
 
 const steps = ['Shipping Adress', 'Payment details'];
 
 const Checkout = () => {
+  const [activeStep, setActiveStep] = useState(0);
+  const classes = useStyles();
   return (
     <>
     <div className={classes.toolbar}/>
