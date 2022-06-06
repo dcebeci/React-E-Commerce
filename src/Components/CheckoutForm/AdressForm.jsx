9852d7f7714e2288ecfeb55dd49ec1175945fe51
dcebeci <cebecidogukan@gmail.com>
2022-06-06 23:48:36 +0300

    düzenleme.

--- src/Components/CheckoutForm/AdressForm.jsx
+++ src/Components/CheckoutForm/AdressForm.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import { InputLabel, Select, MenuItem, Button, Grid, Typographygraphy } from '@material-ui/core'
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core'
 import {useForm, FormProvider} from 'react-hook-form';
-
+import FormInput from './CustomTextField';
 
 
 
 
 const AdressForm=() => {
+  const methods = useForm();
   return (
-    <div>AdressForm</div>
+    <>
+    <Typography variant="h6" gutterBottom>ShippingAdress</Typography>
+    <FormProvider {...methods}>
+      <form onSubmit=''>
+        <Grid container spacing={3}>
+          <FormInput required name='firstName' label= 'First name' />
+        </Grid>
+      </form>
+    </FormProvider>
+    </>
   )
 }
 
-export default AdressForm
+export default AdressForm;
